d0119fa310b325ad729fb79796e8af78fbc9f6b3
kyoon-mit <kyoon@mit.edu>
2026-01-14 17:27:03 -0500

    trainer setup for regression before completing trainer

--- src/BNSReg/dataset/regression_dataset.py
+++ src/BNSReg/dataset/regression_dataset.py
@@ -6,12 +6,14 @@ from BNSReg.utils.config_tools import str_to_dtype
 class BNSDatasetRegression(BNSBaseDataset):
     def __getitem__(self, idx):
         f = self._get_file()
+
         start_idx = self.cfg.window_begin * self.cfg.strain_frequency
         end_idx = self.cfg.window_end * self.cfg.strain_frequency
 
         seq = f['data'][idx, :, start_idx:end_idx:self.cfg.downsample_factor]
-        X_sequence = torch.as_tensor(seq, dtype=str_to_dtype(self.cfg.strain_precision))  # TODO: fix
 
-        y_target = self._get_vars(f, self.cfg.target_variables, idx, dtype=str_to_dtype(self.cfg.variables_precision)) # TODO
-        z_observed = self._get_vars(f, self.cfg.observed_variables, idx, dtype=str_to_dtype(self.cfg.variables_precision)) # TODO
+        X_sequence = torch.as_tensor(seq, dtype=str_to_dtype(self.cfg.strain_precision))
+        y_target = self._get_vars(f, self.cfg.target_variables, idx, dtype=str_to_dtype(self.cfg.variables_precision))
+        z_observed = self._get_vars(f, self.cfg.observed_variables, idx, dtype=str_to_dtype(self.cfg.variables_precision))
+        
         return X_sequence, y_target, z_observed
--- src/BNSReg/tasks/parameter_estimation/model_s4d_mse.py
+++ src/BNSReg/tasks/parameter_estimation/model_s4d_mse.py
@@ -1,131 +1,52 @@
 import torch
-import torch.nn.functional as F
-from torch import nn, optim
+from torch import optim
 from lightning.pytorch import LightningModule
 
-from BNSReg.models.s4d import S4D
+from BNSReg.models.s4d import S4Model
+from BNSReg.core.config import S4DModelConfig
 
-class LitS4DM(LightningModule):
-    def __init__(
-        self,
-        d_input,
-        d_output,
-        variables: list[str],
-        loss='',
-        prenorm=False,
-        d_model=256,
-        n_layers=4,
-        dropout=0.2,
-        lr=0.01,
-        **kwargs
-    ):
+class LitModelS4DMSE(LightningModule):
+    def __init__(self, cfg: S4DModelConfig):
         super().__init__()
-        self.save_hyperparameters()
-        # Do not set self.device directly; Lightning will manage devices.
-        # If you need device info, query `self.device` during `forward` or later.
-        dropout_fn = nn.Dropout1d
-        self.encoder = nn.Linear(d_input, d_model)
-        self.decoder = nn.Linear(d_model, d_output)
-        self.n_outparams = d_output # placeholder
-        # Stack S4 layers as residual blocks
-        self.s4_layers, self.norms, self.dropouts =\
-            nn.ModuleList(), nn.ModuleList(), nn.ModuleList()
-        for _ in range(n_layers):
-            self.s4_layers.append(
-                S4D(d_model, dropout=dropout, transposed=True, lr=lr)
-            )
-            self.norms.append(nn.LayerNorm(d_model))
-            self.dropouts.append(dropout_fn(dropout))
+        self.cfg = cfg
         self.criterion = torch.nn.MSELoss(reduction='mean')
-        # self.criterion = torch.nn.KLDivLoss(reduction='mean')
+        self.model = None
 
-    def __loss__(self, batch):
-        h1, l1, params, idx = batch
-        device = h1.device
-        inputs = torch.stack([h1.to(device), l1.to(device)], dim=2)
-        targets = torch.stack([params[v] for v in self.hparams.variables], dim=1)
-        outputs = self.forward(inputs)
-        loss = self.criterion(outputs, targets)
-        return loss
+    def configure_model(self):
+        if self.model is not None:
+            return
+        else:
+            self.model = S4Model(**self.cfg.model_kwargs())
+            self.model = torch.compile(self.model)
 
     def forward(self, x):
-        """
-        Input x is shape (B, L, d_input)
-        """
-        x = self.encoder(x)  # (B, L, d_input) -> (B, L, d_model)
-        x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
-        for layer, norm, dropout in zip(self.s4_layers, self.norms, self.dropouts):
-            # Each iteration of this loop will map (B, d_model, L) -> (B, d_model, L)
-            z = x
-            if self.hparams.prenorm:
-                # Prenorm
-                z = norm(z.transpose(-1, -2)).transpose(-1, -2)
-            # Apply S4 block: we ignore the state input and output
-            z, _ = layer(z)
-            # Dropout on the output of the S4 block
-            z = dropout(z)
-            # Residual connection
-            x = z + x
-            if not self.hparams.prenorm:
-                # Postnorm
-                x = norm(x.transpose(-1, -2)).transpose(-1, -2)
-        x = x.transpose(-1, -2)
-        # Pooling: average pooling over the sequence length
-        x = x.mean(dim=1)
-        # Decode the outputs
-        x = self.decoder(x)  # (B, d_model) -> (B, d_output)
-        return x
-    
-    def configure_optimizers(self):
-        optimizer = optim.AdamW(self.parameters(), lr=self.optim_lr)
-        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
-        return optimizer, scheduler
+        return self.model(x)
 
-    def on_train_start(self):
-        # free any leftover GPU memory on startup
-        torch.cuda.empty_cache()
+    def compute_loss(self, batch):
+        X_sequence, y_target, z_observed = batch
+        X_sequence = X_sequence.transpose(2, 1)  # (B, d_input, L) -> (B, L, d_input)
+        outputs = self(X_sequence)
+        return self.criterion(outputs, y_target)
+
+    def configure_optimizers(self):
+        optimizer = optim.AdamW(self.parameters(), lr=self.cfg.optim_lr)
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+        return {
+            'optimizer': optimizer,
+            'lr_scheduler': {'scheduler': scheduler, 'interval': 'epoch'},
+        }
 
     def training_step(self, batch, batch_idx):
-        # Clear GPU cache
-        # torch.cuda.empty_cache()
-        loss = self.__loss__(batch)
-        self.log("train/loss",
-            loss,
-            on_step=False,
-            on_epoch=True,
-            reduce_fx='mean',
-            logger=True,
-            prog_bar=True)
+        loss = self.compute_loss(batch)
+        self.log('train/loss', loss, on_step=False, on_epoch=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        # Clear GPU cache
-        # torch.cuda.empty_cache()
-        loss = self.__loss__(batch)
-        self.log("val/loss",
-            loss,
-            on_step=False,
-            on_epoch=True,
-            reduce_fx='mean',
-            logger=True,
-            prog_bar=True)
+        loss = self.compute_loss(batch)
+        self.log('val/loss', loss, on_step=False, on_epoch=True, prog_bar=True)
         return loss
 
     def test_step(self, batch, batch_idx):
-        # Clear GPU cache
-        # torch.cuda.empty_cache()
-        loss = self.__loss__(batch)
-        self.log("test/loss",
-            loss,
-            on_step=False,
-            on_epoch=True,
-            reduce_fx='mean',
-            logger=True,
-            prog_bar=True)
-        return
-
-    def predict_step(self, batch, batch_idx):
-        # Clear GPU cache
-        # torch.cuda.empty_cache()
-        self.test_step(batch, batch_idx)
-        return
+        loss = self.compute_loss(batch)
+        self.log('test/loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
